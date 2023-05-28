3081299bf3cf3a35787d3cb7e4cfdf0943f177b1
Janna Gurtovaia <zhanna.gurt@gmail.com>
2023-05-28 13:37:19 +0200

    delete .DS_Store fron GitHub repo

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,14 @@ const { errors } = require('celebrate');
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('cors');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 })
@@ -35,3 +32,7 @@ app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
